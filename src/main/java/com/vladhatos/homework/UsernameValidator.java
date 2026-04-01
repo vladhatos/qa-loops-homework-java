29303026e6bc7b0f8d9c0f37be4d71bf32063363
unknown <vlad.hatos@gmail.com>
2026-04-02 00:42:35 +0300

    feat:Added UsernameValidator file

---
+++ src/main/java/com/vladhatos/homework/UsernameValidator.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class UsernameValidator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String username;
+        boolean esteValid = false;
+
+        do {
+            System.out.print("Introdu un username: ");
+            username = scanner.nextLine();
+
+            //Verificare spatii
+            boolean numeSpatii = false;
+            for (int i = 0; i < username.length(); i++) {
+                if (username.charAt(i) == ' ') {
+                    numeSpatii = true;
+                    break;
+                }
+            }
+
+            if (numeSpatii) {
+                System.out.println("Username-ul contine spatii");
+                continue;
+            }
+
+            //Verificare lungime nume
+            boolean lungimeNume = username.length() >= 6 && username.length() <= 12;
+
+            //Verificare cifre in nume
+            boolean numeCifra = false;
+            for (int i = 0; i < username.length(); i++) {
+                if (Character.isDigit(username.charAt(i))) {
+                    numeCifra = true;
+                    break;
+                }
+            }
+
+            // Validare finală pentru regulile rămase
+            if (!lungimeNume) {
+                System.out.println("Eroare: Lungimea trebuie sa fie intre 6 și 12 caractere");
+            } else if (!numeCifra) {
+                System.out.println("Eroare: Username-ul trebuie sa contina cel putin o cifra");
+            } else {
+                esteValid = true;
+            }
+
+        } while (!esteValid);
+
+        System.out.println("username acceptat: " + username);
+        scanner.close();
+    }
+}
